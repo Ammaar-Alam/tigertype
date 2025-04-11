d7f2d6f2a57bedfec7340bc8558c5a1891b40e7c
Ryan Chen <rc6542@princeton.edu>
2025-04-11 01:24:03 -0400

    ?

--- client/src/components/Sound.jsx
+++ client/src/components/Sound.jsx
@@ -5,7 +5,6 @@ const keySound = new Audio(typingSound);
 function playKeySound() {
     const shouldPlaySound = localStorage.getItem('typingSound') === 'true';
     if (shouldPlaySound) {
-
         keySound.currentTime = 0;
         keySound.volume = 0.2;
         keySound.play();
